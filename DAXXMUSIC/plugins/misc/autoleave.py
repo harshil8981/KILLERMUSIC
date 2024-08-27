8155f2bbe9c3969862f1fe7d8b3d37f1b5c6e38c
Mr Killer 1109 <69214079+harshil8981@users.noreply.github.com>
2024-08-27 16:58:54 +0530

    Update autoleave.py

--- DAXXMUSIC/plugins/misc/autoleave.py
+++ DAXXMUSIC/plugins/misc/autoleave.py
@@ -30,8 +30,8 @@ async def auto_leave():
                             chat_id = i.chat.id
                             if (
                                 chat_id != config.LOGGER_ID
-                                and i.chat.id != -1001919135283
-                                and i.chat.id != -1001841879487
+                                and i.chat.id != -1001719051389
+                                and i.chat.id != -1001719051389
                             ):
                                 if left == 20:
                                     continue
