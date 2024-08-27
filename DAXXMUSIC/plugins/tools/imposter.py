af40a8bd185c1fe85724af0729eb07d0cb5ec899
Mr Killer 1109 <69214079+harshil8981@users.noreply.github.com>
2024-08-27 17:09:38 +0530

    Update imposter.py

--- DAXXMUSIC/plugins/tools/imposter.py
+++ DAXXMUSIC/plugins/tools/imposter.py
@@ -30,8 +30,8 @@ ROY = [
     [
         InlineKeyboardButton(
             text="ᴀᴅᴅ ᴍᴇ",
-            url=f"https://t.me/NexikoBot?startgroup=true"),
-        InlineKeyboardButton(text="ᴜᴘᴅᴀᴛᴇ", url=f"https://t.me/gitwizardbypass")
+            url=f"https://t.me/MRKILLERMUSIC_BOT?startgroup=true"),
+        InlineKeyboardButton(text="ᴜᴘᴅᴀᴛᴇ", url=f"https://t.me/Hpbot_update")
     ],
 ]
 
